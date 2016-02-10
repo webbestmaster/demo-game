6b2c15017339fa50247697a1b0e711c2a207bb1b
ganyna <ganyna@mail.ru>
2016-02-10 17:59:12 +0300

    dist server - show ip and port

--- dist-server/server.js
+++ dist-server/server.js
@@ -1,7 +1,10 @@
 var express = require('express'),
 	app = express(),
 	helper = require('./helper'),
-	sendFile = helper.sendFile;
+	sendFile = helper.sendFile,
+	os = require('os'),
+	ifaces = os.networkInterfaces(),
+	port = process.env.PORT || 3000;
 
 //app.use(express.bodyParser());
 
@@ -67,4 +70,28 @@ app.use(function (error, req, res, next) {
 	res.status(500).send('500: Internal Server Error');
 });
 
-app.listen(process.env.PORT || 3000);
+app.listen(port);
+
+
+
+// show IP and port
+Object.keys(ifaces).forEach(function (ifname) {
+	var alias = 0;
+
+	ifaces[ifname].forEach(function (iface) {
+		if ('IPv4' !== iface.family || iface.internal !== false) {
+			// skip over internal (i.e. 127.0.0.1) and non-ipv4 addresses
+			return;
+		}
+
+		if (alias >= 1) {
+			// this single interface has multiple ipv4 addresses
+			console.log(ifname + ':' + alias, iface.address);
+		} else {
+			// this interface has only one ipv4 adress
+			//console.log(ifname, iface.address);
+			console.log('dist server started on http://' + iface.address + ':' + port + '/');
+		}
+		++alias;
+	});
+});
