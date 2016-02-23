20224f345b1f709c3e38cb3de4e9992dc30c47bb
Andrey Poznyak <andrey.poznyak@viaden.com>
2016-02-24 01:32:44 +0300

    no message

--- webpack.config.js
+++ webpack.config.js
@@ -4,11 +4,15 @@ module.exports = {
     entry: __dirname + "/www/js/main",
     resolve: {
         modulesDirectories: [
-            "."
-        ]
+            ""
+        ],
+        alias: {
+            //'Deferred': "./js/deferred"
+        }
     },
     output: {
         path: __dirname + "/dist/www/js/",
-        filename: "main.js"
+        filename: "main.js",
+        publicPath: "./js/"
     }
 };
